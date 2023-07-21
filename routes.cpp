11f7956527931801a2da9dc3a3cb8e76b7b6167e
SAHIL VERMA <116958617+sahil-1702@users.noreply.github.com>
2023-07-22 03:50:57 +0530

    updated

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,29 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello world!"};
 }
 crow::response add(const crow::request &req)
 {
-    auto input = crow::json::load(req.body);
-    return crow::response{""};
+   auto input = crow::json::load(req.body);
+    int first = input["first"].i();
+    int second = input["second"].i();
+    int result = first + second;
+
+    crow::json::wvalue response;
+    response["result"] = result;
+
+    return crow::response{response};
 }
 crow::response subtract(const crow::request &req)
 {
-    auto input = crow::json::load(req.body);
-    return crow::response{""};
+   auto input = crow::json::load(req.body);
+    int first = input["first"].i();
+    int second = input["second"].i();
+    int result = first - second;
+
+    crow::json::wvalue response;
+    response["result"] = result;
+
+    return crow::response{response};
 }
